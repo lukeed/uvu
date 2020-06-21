94e31d6b105595345f4e04423ab38e23b7bc2808
Luke Edwards <luke.edwards05@gmail.com>
2020-06-20 17:03:39 -0700

    feat: extract `parse` module

--- bin.js
+++ bin.js
@@ -1,19 +1,8 @@
 #!/usr/bin/env node
 const sade = require('sade');
-const tlist = require('totalist');
-const { resolve } = require('path');
+const parse = require('./parse');
 const { exec, QUEUE } = require('.');
 const pkg = require('./package');
-
-const toRegex = x => new RegExp(x, 'i');
-
-function exists(dep) {
-	try {
-		return require.resolve(dep);
-	} catch (err) {
-		return false;
-	}
-}
 
 sade('uvu [dir] [pattern]')
 	.version(pkg.version)
@@ -22,26 +11,7 @@ sade('uvu [dir] [pattern]')
 	.option('-r, --require', 'Additional module(s) to preload')
 	.option('-C, --cwd', 'The current directory to resolve from', '.')
 	.action(async (dir, pattern, opts) => {
-		let suites = [];
-		if (pattern) pattern = toRegex(pattern);
-		else if (dir) pattern = /(((?:[^\/]*(?:\/|$))*)[\\\/])?\w+\.([mc]js|[jt]sx?)$/;
-		else pattern = /((\/|^)(tests?|__tests?__)\/.*|\.(tests?|spec)|^\/?tests?)\.([mc]js|[jt]sx?)$/i;
-		dir = resolve(opts.cwd, dir || '.');
-
-		let ignores = ['node_modules'].concat(opts.ignore || []).map(toRegex);
-
-		[].concat(opts.require || []).filter(Boolean).forEach((name, tmp) => {
-			if (tmp = exists(name)) return require(tmp);
-			throw new Error(`Cannot find module '${name}'`);
-		});
-
-		await tlist(dir, (rel, abs) => {
-			if (pattern.test(rel) && !ignores.some(x => x.test(rel))) {
-				suites.push({ name: rel, file: abs });
-			}
-		});
-
-		suites.sort((a, b) => a.name.localeCompare(b.name));
+		let { suites } = await parse(dir, pattern, opts);
 
 		globalThis.UVU_DEFER = 1;
 		suites.forEach((x, idx) => {
---
+++ src/parse.js
@@ -0,0 +1,38 @@
+import { resolve } from 'path';
+import tlist from 'totalist';
+
+const toRegex = x => new RegExp(x, 'i');
+
+function exists(dep) {
+	try {
+		return require.resolve(dep);
+	} catch (err) {
+		return false;
+	}
+}
+
+export default async function (dir, pattern, opts = {}) {
+	if (pattern) pattern = toRegex(pattern);
+	else if (dir) pattern = /(((?:[^\/]*(?:\/|$))*)[\\\/])?\w+\.([mc]js|[jt]sx?)$/;
+	else pattern = /((\/|^)(tests?|__tests?__)\/.*|\.(tests?|spec)|^\/?tests?)\.([mc]js|[jt]sx?)$/i;
+	dir = resolve(opts.cwd, dir || '.');
+
+	[].concat(opts.require || []).filter(Boolean).forEach(name => {
+		let tmp = exists(name);
+		if (tmp) return require(tmp);
+		throw new Error(`Cannot find module '${name}'`);
+	});
+
+	let suites = [];
+	let ignores = ['node_modules'].concat(opts.ignore || []).map(toRegex);
+
+	await tlist(dir, (rel, abs) => {
+		if (pattern.test(rel) && !ignores.some(x => x.test(rel))) {
+			suites.push({ name: rel, file: abs });
+		}
+	});
+
+	suites.sort((a, b) => a.name.localeCompare(b.name));
+
+	return { dir, suites };
+}
