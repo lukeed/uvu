b525fa9636f0d36b9718e38d72888a39efbbb886
Luke Edwards <luke.edwards05@gmail.com>
2020-06-22 16:59:46 -0700

    feat: rudimentary `diff.arrays` output

--- src/diff.js
+++ src/diff.js
@@ -9,32 +9,53 @@ const colors = {
 
 const TITLE = kleur.dim().italic;
 const TAB=kleur.dim('→'), SPACE=kleur.dim('·'), NL=kleur.dim('↵');
+const LOG = (sym, str) => colors[sym](sym + PRETTY(str)) + '\n';
 const LINE = (num, x) => kleur.dim('L' + String(num).padStart(x, '0') + ' ');
 const PRETTY = str => str.replace(/[ ]/g, SPACE).replace(/\t/g, TAB).replace(/(\r?\n)/g, NL);
 const PRINT = (sym, str, len) => colors[sym](sym + str + ' '.repeat(4 + len) + TITLE(sym == '++' ? '(Expected)\n' : '(Actual)\n'));
 
 function line(obj, prev, pad) {
 	let char = obj.removed ? '--' : obj.added ? '++' : '··';
-	let i=0, tmp, arr = obj.value.replace(/\r?\n$/, '').split('\n');
-	let out='', fmt = colors[char];
+	let arr = obj.value.replace(/\r?\n$/, '').split('\n');
+	let i=0, tmp, out='';
 
-	if (obj.added) out += fmt().underline(TITLE('Expected:')) + '\n';
-	else if (obj.removed) out += fmt().underline(TITLE('Actual:')) + '\n';
+	if (obj.added) out += colors[char]().underline(TITLE('Expected:')) + '\n';
+	else if (obj.removed) out += colors[char]().underline(TITLE('Actual:')) + '\n';
 
 	for (; i < arr.length; i++) {
 		tmp = arr[i];
 		if (tmp != null) {
 			if (prev) out += LINE(prev + i, pad);
-			out += fmt(char + PRETTY(tmp || '\n')) + '\n';
+			out += LOG(char, tmp || '\n');
 		}
 	}
 
 	return out;
 }
 
-// TODO
+// TODO: want better diffing
+//~> complex items bail outright
 export function arrays(input, expect) {
 	let arr = diff.diffArrays(input, expect);
+	let i=0, j=0, k=0, tmp, val, char;
+	let out = LOG('··', '[');
+
+	for (; i < arr.length; i++) {
+		char = (tmp = arr[i]).removed ? '--' : tmp.added ? '++' : '··';
+
+		if (tmp.added) {
+			out += colors[char]().underline(TITLE('Expected:')) + '\n';
+		} else if (tmp.removed) {
+			out += colors[char]().underline(TITLE('Actual:')) + '\n';
+		}
+
+		for (j=0; j < tmp.value.length; j++) {
+			val = JSON.stringify(tmp.value[j], null, 2).split(/\r?\n/g);
+			for (k=0; k < val.length; k++) out += LOG(char, '  ' + val[k] + (/^[{}]$/.test(val[k]) ? '' : ','));
+		}
+	}
+
+	return out + LOG('··', ']');
 }
 
 export function lines(input, expect, linenum = 0) {
