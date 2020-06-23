45450c2424044642d334bbc3cd0920b62262cd05
Luke Edwards <luke.edwards05@gmail.com>
2020-06-22 18:34:58 -0700

    chore: initial `diff.direct` tests

--- test/diff.js
+++ test/diff.js
@@ -2,9 +2,7 @@ import { suite } from 'uvu';
 import * as assert from 'uvu/assert';
 import * as $ from '../src/diff';
 
-function strip(stdout) {
-	return stdout.replace(/[\u001B\u009B][[\]()#;?]*(?:(?:(?:[a-zA-Z\d]*(?:;[-a-zA-Z\d\/#&.:=?%@~_]*)*)?\u0007)|(?:(?:\d{1,4}(?:;\d{0,4})*)?[\dA-PR-TZcf-ntqry=><~]))/g, '');
-}
+const strip = str => str.replace(/[\u001B\u009B][[\]()#;?]*(?:(?:(?:[a-zA-Z\d]*(?:;[-a-zA-Z\d\/#&.:=?%@~_]*)*)?\u0007)|(?:(?:\d{1,4}(?:;\d{0,4})*)?[\dA-PR-TZcf-ntqry=><~]))/g, '');
 
 const arrays = suite('arrays');
 
@@ -40,6 +38,52 @@ const direct = suite('direct');
 
 direct('should be a function', () => {
 	assert.type($.direct, 'function');
+});
+
+// TODO: return nothing ?
+direct.skip('should no differences with identical text', () => {
+	// assert.fixture();
+});
+
+direct('should handle `"foo"` vs `"fo"` diff', () => {
+	assert.snapshot(
+		strip($.direct('foo', 'fo')),
+		'++fo     (Expected)\n' +
+		'--foo    (Actual)\n'
+	);
+});
+
+direct('should handle `"fo"` vs `"foo"` diff', () => {
+	assert.snapshot(
+		strip($.direct('fo', 'foo')),
+		'++foo    (Expected)\n' +
+		'--fo     (Actual)\n'
+	);
+});
+
+direct('should handle `"foo"` vs `"bar"` diff', () => {
+	assert.snapshot(
+		strip($.direct('foo', 'bar')),
+		'++bar    (Expected)\n' +
+		'--foo    (Actual)\n'
+	);
+});
+
+direct('should handle `123` vs `456` diff', () => {
+	assert.snapshot(
+		strip($.direct(123, 456)),
+		'++456    (Expected)\n' +
+		'--123    (Actual)\n'
+	);
+});
+
+// TODO: show type difference
+direct('should handle `123` vs `"123"` diff', () => {
+	assert.snapshot(
+		strip($.direct(123, '123')),
+		'++123    (Expected)\n' +
+		'--123    (Actual)\n'
+	);
 });
 
 direct.run();
