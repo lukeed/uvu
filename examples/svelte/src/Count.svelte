7ffa304b7bd8f81ef2fdc75aa30a4f6b7085c62d
Luke Edwards <luke.edwards05@gmail.com>
2020-07-13 15:54:07 -0700

    chore: add "svelte" demo (#18)
    * fix(parse): allow local require hooks
    
    * chore: add "svelte" example
    
    * chore: jsdoc
    
    * fix: add missing `--ignore` flag
    
    * fix: remove assertions on component internals
    
    * chore: modify `ENV.render` output

---
+++ examples/svelte/src/Count.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let count = 5;
+
+	function increment() {
+		count++;
+	}
+
+	function decrement() {
+		count--;
+	}
+</script>
+
+<button id="decr" on:click={decrement}>--</button>
+<span>{count}</span>
+<button id="incr" on:click={increment}>++</button>
