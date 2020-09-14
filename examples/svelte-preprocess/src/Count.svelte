14eb6446b43f5f8736de46a2205889f31091321e
tommywalkie <tommywalkie@gmail.com>
2020-09-14 12:25:30 +0200

    Add Svelte TypeScript preprocessing example

---
+++ examples/svelte-preprocess/src/Count.svelte
@@ -0,0 +1,16 @@
+<script>
+    type CountResult = number
+    export let count: CountResult = 5;
+
+    function increment(): void {
+        count++;
+    }
+
+    function decrement(): void {
+        count--;
+    }
+</script>
+
+<button id="decr" on:click={decrement}>--</button>
+<span>{count}</span>
+<button id="incr" on:click={increment}>++</button>
