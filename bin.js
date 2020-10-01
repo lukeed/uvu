973eb0481d89b922b7ad4b6971dc5ea5181c69ab
Luke Edwards <luke.edwards05@gmail.com>
2020-10-01 13:56:50 -0700

    fix(bin): exit w/ non-zero code on boot/require error;
    - Closes #50

--- bin.js
+++ bin.js
@@ -11,18 +11,23 @@ sade('uvu [dir] [pattern]')
 	.option('-C, --cwd', 'The current directory to resolve from', '.')
 	.option('-c, --color', 'Print colorized output', true)
 	.action(async (dir, pattern, opts) => {
-		if (opts.color) process.env.FORCE_COLOR = '1';
-		let { suites } = await parse(dir, pattern, opts);
-		let { exec, QUEUE } = require('.');
+		try {
+			if (opts.color) process.env.FORCE_COLOR = '1';
+			let { suites } = await parse(dir, pattern, opts);
+			let { exec, QUEUE } = require('.');
 
-		// TODO: mjs vs js file
-		globalThis.UVU_DEFER = 1;
-		suites.forEach((x, idx) => {
-			globalThis.UVU_INDEX = idx;
-			QUEUE.push([x.name]);
-			require(x.file); // auto-add to queue
-		});
+			// TODO: mjs vs js file
+			globalThis.UVU_DEFER = 1;
+			suites.forEach((x, idx) => {
+				globalThis.UVU_INDEX = idx;
+				QUEUE.push([x.name]);
+				require(x.file); // auto-add to queue
+			});
 
-		await exec(opts.bail);
+			await exec(opts.bail);
+		} catch (err) {
+			console.error(err.stack || err.message);
+			process.exit(1);
+		}
 	})
 	.parse(process.argv);
