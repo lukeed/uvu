25fb0fd644095d5b015bea112e9106f4ea64dc0f
Luke Edwards <luke.edwards05@gmail.com>
2020-11-30 10:34:14 -0800

    chore: code style

--- examples/puppeteer/tests/puppeteer.js
+++ examples/puppeteer/tests/puppeteer.js
@@ -1,40 +1,39 @@
-import { suite } from 'uvu'
-import * as assert from 'uvu/assert'
-import puppeteer from 'puppeteer'
+import { suite } from 'uvu';
+import * as assert from 'uvu/assert';
+import Chrome from 'puppeteer';
 
-const Puppeteer = suite('puppeteer')
+const puppeteer = suite('puppeteer');
 
-// Launch the browser, then add the browser and page to your
-// suite's context for easy access in tests.
-Puppeteer.before(async context => {
-  const browser = await puppeteer.launch(),
-        page = (await browser.pages())[0]
+// Launch the browser
+// Add `browser` and `page` to context
+puppeteer.before(async context => {
+	context.browser = await Chrome.launch();
+	context.page = await context.browser.newPage();
+});
 
-  context.puppeteer = { browser, page }
+// Close everything on suite completion
+puppeteer.after(async context => {
+	await context.page.close();
+	await context.browser.close();
+});
+
+puppeteer('can fetch data!', async context => {
+  const data = await context.page.evaluate(() => {
+    return fetch('https://httpbin.org/get').then(r => r.json());
+	});
+	assert.type(data, 'object');
+	assert.is(data.url, 'https://httpbin.org/get');
 })
 
-// Close the browser after all tests have run.
-Puppeteer.after(async ({ puppeteer: { browser }}) => {
-  await browser.close()
-})
+puppeteer('can select elements!', async context => {
+  await context.page.goto('http://example.com/');
 
-Puppeteer('can fetch data!', async ({ puppeteer: { page } }) => {
-  const fetchedData = await page.evaluate(async () => {
-    const result = await fetch('https://httpbin.org/get')
-    return await result.json()
-  })
+  const text = await context.page.evaluate(() => {
+    return document.querySelector('h1').textContent;
+  });
 
-  assert.ok(fetchedData)
-})
+  assert.type(text, 'string');
+  assert.is(text, 'Example Domain');
+});
 
-Puppeteer('can select elements!', async ({ puppeteer: { page } }) => {
-  await page.goto('http://example.com/')
-  
-  const textContent = await page.evaluate(() => {
-    return document.querySelector('h1').textContent
-  })
-
-  assert.ok(textContent)
-})
-
-Puppeteer.run()
+puppeteer.run();
