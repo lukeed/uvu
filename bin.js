01c70d40f6669e7e52835fff111f0dc18d2a8585
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-03-17 15:32:48 -0400

    feat: add --watch flag
    You must install uvu-watch globally to use it.

--- bin.js
+++ bin.js
@@ -12,12 +12,26 @@ const hasImport = (() => {
 
 sade('uvu [dir] [pattern]')
 	.version(pkg.version)
+	.option('-w, --watch', 'Rerun tests on file changes')
 	.option('-b, --bail', 'Exit on first failure')
 	.option('-i, --ignore', 'Any file patterns to ignore')
 	.option('-r, --require', 'Additional module(s) to preload')
 	.option('-C, --cwd', 'The current directory to resolve from', '.')
 	.option('-c, --color', 'Print colorized output', true)
 	.action(async (dir, pattern, opts) => {
+		if (opts.watch) {
+			const { spawnSync } = require('child_process');
+			const npxArgs = '-q --no-install uvu-watch'.split(' ');
+			const argv = npxArgs.concat(process.argv.slice(2));
+			const { status } = spawnSync('npx', argv, {
+				stdio: 'inherit',
+			});
+			if (status == 127) {
+				const { red } = require('kleur');
+				console.error(red('[!]'), '"uvu-watch" must be installed');
+			}
+			process.exit(status);
+		}
 		try {
 			if (opts.color) process.env.FORCE_COLOR = '1';
 			let ctx = await parse(dir, pattern, opts);
