d56753ec20928da1c22ff093cb07ce3c67613706
osdevisnot <osdevisnot@gmail.com>
2021-07-13 00:24:36 -0700

    chore: add "esbuild" example (#119)
    * chore: add esbuild example
    
    * chore: remove `.gitignore` file
    
    Co-authored-by: Luke Edwards <luke.edwards05@gmail.com>

---
+++ examples/esbuild/tests/utils.ts
@@ -0,0 +1,18 @@
+import { test } from 'uvu';
+import * as assert from 'uvu/assert';
+import * as utils from '../src/utils';
+
+test('capitalize', () => {
+	assert.type(utils.capitalize, 'function');
+	assert.is(utils.capitalize('hello'), 'Hello');
+	assert.is(utils.capitalize('foo bar'), 'Foo bar');
+});
+
+test('dashify', () => {
+	assert.type(utils.dashify, 'function');
+	assert.is(utils.dashify('fooBar'), 'foo-bar');
+	assert.is(utils.dashify('FooBar'), 'foo-bar');
+	assert.is(utils.dashify('foobar'), 'foobar');
+});
+
+test.run();
