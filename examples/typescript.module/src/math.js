6632724422f02294b3dd113a964456fc79e909d5
Luke Edwards <luke.edwards05@gmail.com>
2021-10-04 23:22:27 -0700

    chore(examples): add "typescript.module" demo;
    - Similar to "typescript" example, but uses `"type": "module"` and source files only include ".js" files with ESM syntax. This matches the scenario(s) presented in #123
    
    Also includes two approaches for solving this problem; see npm "scripts" section.
    
    Closes #123

---
+++ examples/typescript.module/src/math.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ */
+export const sum = (a, b) => a + b;
+
+/**
+ * @param {number} a
+ * @param {number} b
+ */
+export const div = (a, b) => a / b;
+
+/**
+ * @param {number} a
+ * @param {number} b
+ */
+export const mod = (a, b) => a % b;
