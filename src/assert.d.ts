a6b84cacf68fa9ef46694e0901ef1f70c41921df
Andrey Sitnik <andrey@sitnik.ru>
2023-09-06 15:18:27 +0200

    Connect `actual` and `expects` types in `equal()` and `is()`

--- src/assert.d.ts
+++ src/assert.d.ts
@@ -1,9 +1,21 @@
 type Types = 'string' | 'number' | 'boolean' | 'object' | 'undefined' | 'function';
 
+declare interface Is {
+  <Actual, Expects extends Actual>(actual: Actual, expected: Expects, message?: string): void;
+	not(actual: any, expects: any, msg?: Message): void;
+}
+
+declare interface Equal {
+  <Actual, Expects extends Actual>(actual: Actual, expected: Expects, message?: string): void;
+	<Actual extends Expects, Expects>(actual: Actual, expected: Expects, message?: string): void;
+	<Actual, Expects>(actual: Actual, expected: Expects, message?: string): boolean;
+	skip(actual: any, expected: any, message?: string): void;
+}
+
 export type Message = string | Error;
+export const equal: Equal;
+export const is: Is;
 export function ok(actual: any, msg?: Message): asserts actual;
-export function is(actual: any, expects: any, msg?: Message): void;
-export function equal(actual: any, expects: any, msg?: Message): void;
 export function type(actual: any, expects: Types, msg?: Message): void;
 export function instance(actual: any, expects: any, msg?: Message): void;
 export function snapshot(actual: string, expects: string, msg?: Message): void;
@@ -12,10 +24,6 @@ export function match(actual: string, expects: string | RegExp, msg?: Message): 
 export function throws(fn: Function, expects?: Message | RegExp | Function, msg?: Message): void;
 export function not(actual: any, msg?: Message): void;
 export function unreachable(msg?: Message): void;
-
-export namespace is {
-	function not(actual: any, expects: any, msg?: Message): void;
-}
 
 export namespace not {
 	function ok(actual: any, msg?: Message): void;
